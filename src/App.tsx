bff8566c743871a1b9fc2ea58a6ec3198689694b
PathonScript <pathonscript@gmail.com>
2021-09-28 10:58:53 +0700

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Quote />
+      <QuoteForm />
     </div>
-  );
+  )
 }
+
+
+function Quote() {
+  let [quote, setQuote] = useState('');
+  let prevQuote: string;
+  let dummyQuote: string;
+
+  const quoteArr = [
+    'Just killed a woman, feeling good.',
+    'Mum, am I adoted?',
+    'I want a pet moth and I want to name it ✨Clementine✨'
+  ]
+  
+  const nextQuote = async (e: any) => {
+    e.preventDefault();
+    
+    prevQuote = quote;
+    dummyQuote = quoteArr[Math.floor(Math.random() * quoteArr.length)]
+    while(prevQuote === dummyQuote) {
+      dummyQuote = quoteArr[Math.floor(Math.random() * quoteArr.length)]
+    }
+    setQuote(quote = dummyQuote)
+    
+    console.log(`${dummyQuote}\n${quote}\n${prevQuote}`)
+  }
+  return (
+    <div className="quote-container">
+      <div className="container">
+        <h1 className="quote">{quote}</h1>
+        <div className="break"></div>
+        <button onClick={nextQuote}>:)</button>
+      </div>
+    </div>
+  )
+}
+
+function QuoteForm() {
+  return (
+    <div className="form-container">
+      <div className="container">
+        <form className="quote-form">
+          <p className='quote-input'>Have new quotes to share?</p>
+          <div className="break"></div>
+          <input className="quote-input"type="text" placeholder='Type your quote here'/>
+          <div className="break"></div>
+          <input className='quote-file'type='file' />
+          <div className="break"></div>
+          <button className='quote-button' type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
 
 export default App;
